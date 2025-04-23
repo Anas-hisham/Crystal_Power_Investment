17dc6c8db73e0991ffd93b47f4c49c3ac5f196bf
Anas Hisham <anshisham123@gmail.com>
2025-04-23 23:17:56 +0200

    Rewrite <filename> with new content

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['lh3.googleusercontent.com','fakestoreapi.com'], // allow Google profile pictures
-  }, 
+    domains: ['lh3.googleusercontent.com', 'fakestoreapi.com'],
+  },
 };
 
 export default nextConfig;
